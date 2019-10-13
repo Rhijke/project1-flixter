2aa60701ba6a9f21b03f9bf349bcc13699843400
Kaerah Lopez <kaerah.lopez@gmail.com>
2019-10-13 12:54:33 -0500

    Retrieve data from movie API and encapsulate in Movie objects

--- app/src/main/java/android/example/com/flixter/MainActivity.java
+++ app/src/main/java/android/example/com/flixter/MainActivity.java
@@ -2,14 +2,55 @@ package android.example.com.flixter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.example.com.flixter.models.Movie;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + BuildConfig.MOVIE_API_KEY;
+    public static final String TAG = "MainActivity";
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "Success API");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSON error", e);
+
+                } catch (Exception e) {
+                    Log.e(TAG, "Movie error", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "Fail API");
+            }
+        });
     }
 }
